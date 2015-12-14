81e2913436b3ada6715b78d70b28c83f487650cc
junjiegao <jgao@student.tgm.ac.at>
2015-12-14 13:58:21 +0100

    Zusammengefügt

---
+++ LightOut/src/Light.java
@@ -0,0 +1,17 @@
+import java.awt.Color;
+
+public class Light extends JButton{
+	boolean lon;
+	int x;
+	int y;
+	public Light(int x,int y, boolean lon){
+		this.lon=lon;
+		this.x=x;
+		this.y=y;
+	}
+	public void changeCol(){
+		lon != lon;
+		if(lon)this.setBackground(Color.YELLOW);
+		else this.setBackground(Color.BLACK);
+	}
+}
